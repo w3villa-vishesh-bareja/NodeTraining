25d7d3726ed4206a5a20e758ca6a1a88552442ca
w3villa-vishesh-bareja <vishesh.bareja@w3villa.com>
2025-07-21 12:15:49 +0530

    cloud redis

--- config/redis.js
+++ config/redis.js
@@ -1,7 +1,17 @@
 import { createClient } from 'redis';
-const redis = createClient({url: 'redis://localhost:6379'});
+import { configDotenv } from 'dotenv';
+configDotenv();
+const redis = createClient({
+    username: process.env.REDIS_USERNAME,
+    password: process.env.REDIS_PASSWORD,
+    socket: {
+        host: process.env.REDIS_HOST,
+        port: process.env.REDIS_PORT
+    }
+});
 try {
     await redis.connect();
+    console.log("redis connected")
 } catch (error) {
     throw new Error(error)
 }
